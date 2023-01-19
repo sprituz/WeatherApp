eb6f583ffcf3b840e4b0e819f8ebc48fb0fbbf69
Dayeon Lee <dayeon2768@daum.net>
2023-01-19 19:00:37 +0900

    [FEAT] Setup project structure

--- WeatherApp/ViewController/HomeViewController.swift
+++ WeatherApp/ViewController/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  WeatherApp
 //
 //  Created by 이다연 on 2023/01/19.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
