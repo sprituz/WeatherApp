74716bbebdab8bbfa71680008484054590a584e2
Dayeon Lee <dayeon2768@daum.net>
2024-02-05 15:07:24 +0900

    Feat: Add SearchViewController

--- WeatherApp/WeatherApp/ViewControllers/SearchViewController.swift
+++ WeatherApp/WeatherApp/ViewControllers/SearchViewController.swift
@@ -6,10 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController/*, UISearchBarDelegate, UISearchResultsUpdating*/ {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        <#code#>
+//    }
+//    
+    
+    private let searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        configureUI()
     }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+//        let searchController = UISearchController(searchResultsController: nil)
+//        searchController.searchBar.placeholder = "검색(placeholder)"
+//        // 내비게이션 바는 항상 표출되도록 설정
+//        searchController.hidesNavigationBarDuringPresentation = false
+//        /// updateSearchResults(for:) 델리게이트를 사용을 위한 델리게이트 할당
+//        searchController.searchResultsUpdater = self
+//        /// 뒷배경이 흐려지지 않도록 설정
+//        searchController.obscuresBackgroundDuringPresentation = false
+//        
+//        navigationItem.searchController = searchController
+//        navigationItem.hidesSearchBarWhenScrolling = false
+        
+    }
+    
 }
