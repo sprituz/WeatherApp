b38c294685d78c22cb056c093535c45096662ed9
Dayeon Lee <dayeon2768@daum.net>
2024-02-21 08:09:06 +0900

    Test: Hourly weather data test

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -9,6 +9,28 @@ import XCTest
 @testable import WeatherApp
 
 final class WeatherAppTests: XCTestCase {
+    
+    var apiService = APIService.shared
+    
+
+    func testFetchData() {
+        // 이곳에 API를 호출하는 코드를 작성해주세요.
+        // 예를 들면, apiService.fetchData() 같은 형태가 될 수 있습니다.
+
+        // XCTestExpectation을 생성합니다.
+        let expectation = self.expectation(description: "fetching data")
+
+        apiService.getHourlyWeather(city: "Seoul").subscribe(onNext: { data in
+            XCTAssertNotNil(data, "data should not be nil")
+            expectation.fulfill() // API 호출이 성공했음을 알립니다.
+        }, onError: { error in
+            XCTFail("error occured: \(error.localizedDescription)")
+            expectation.fulfill() // API 호출이 실패했음을 알립니다.
+        })
+
+        // 비동기 테스트를 위한 타임아웃을 설정합니다.
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
