4fbc31e0e7d2bbafe78b213d33a07dc65434a99f
Dayeon Lee <dayeon2768@daum.net>
2024-04-22 14:08:52 +0900

    Refector: API KEY leak fix

--- WeatherApp/WeatherApp/Views/WeatherTileOverlay.swift
+++ WeatherApp/WeatherApp/Views/WeatherTileOverlay.swift
@@ -9,12 +9,12 @@ import UIKit
 import MapKit
 
 class WeatherTileOverlay: MKTileOverlay {
-    let APIKey = "YOUR_OPENWEATHERMAP_API_KEY"
+    let appid = Bundle.main.WEATHER_API_KEY
     
     override func url(forTilePath path: MKTileOverlayPath) -> URL {
         let xTileShift = 1 << path.z // 2^z
         let yTile = xTileShift - path.y - 1 // convert y coordinate
-        let urlStr = "https://tile.openweathermap.org/map/temp_new/\(path.z)/\(path.x)/\(yTile).png?appid=***REMOVED***"
+        let urlStr = "https://tile.openweathermap.org/map/temp_new/\(path.z)/\(path.x)/\(yTile).png?appid=\(appid)"
         return URL(string: urlStr)!
     }
 }
