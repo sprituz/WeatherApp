79d43e2a2754c0a0cb197a62d2ba11f811d77595
Dayeon Lee <dayeon2768@daum.net>
2024-09-04 09:21:53 +0900

    Refactor: Search Location with MapKit

--- WeatherApp/WeatherApp/Models/Coord.swift
+++ WeatherApp/WeatherApp/Models/Coord.swift
@@ -7,13 +7,6 @@
 
 import Foundation
 
-struct Location: Codable {
-    let id: Int
-    let name: String
-    let country: String
-    let coord: Coord
-}
-
 struct Coord: Codable,Equatable {
     let lon: Double?
     let lat: Double?
